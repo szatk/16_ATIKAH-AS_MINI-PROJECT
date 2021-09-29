99fc5db25c56ad32f17ace0b5e918ab29f7b6748
Atikah A. Siregar <89565846+szatk@users.noreply.github.com>
2021-09-29 18:09:17 +0700

    tampilan

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -8,28 +8,30 @@ import '../assets/css/home.css'
 
 function Home() {
     return (
-       <div>  
-      <Navbar />
       <div id="my-content">
+      <Navbar />
 
       <div className="header-body d-flex justify-content-center ">
-        <div className="container mt-1">
+        <div className="container mt-5">
           <div className="row mb-5">
-            {/* <p className="timestamp">{timestamp}</p> */}
+            {/* <h1 style={{ margin: 'auto', textAlign: 'center' }}>{timestamp}</h1> */}
           </div>
           <div className="row">
-         
-            <div className="col-md-8 ">
-              <img className="my-img" src={background} alt="profile-pic justify-content-center" />
+            <div className="col-md-12">
+              <img className="my-img" src={background} alt="profile-pic" />
             </div>
-            <div className="col-md-4">
+                <NavLink to="/contact">
+                  <button
+                    type="button"
+                    className="btn btn-primary btn-lg btn-getInTouch"
+                  >
+                    Get In Touch
+                  </button>
+                </NavLink>
               </div>
-           
             </div>
           </div>
         </div>
-      </div>
-      </div>   
   )
 }
 
